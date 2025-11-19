f0841d1733794c6dfc16754e4e594025335d318a
Hung Tran <hung.tran@smulie.io>
2025-11-19 20:46:21 +0700

    fix bugg

--- src/train.py
+++ src/train.py
@@ -13,8 +13,8 @@ from tqdm import tqdm
 from constants import DATA_JSON
 
 # ---------- Config ----------
-APPEARANCE_DIR = "features_v2/appearance"
-MOTION_DIR = "features_v2/motion"
+APPEARANCE_DIR = "/kaggle/working/features_v2/appearance"
+MOTION_DIR = "/kaggle/working/features_v2/motion"
 # train.py
 MODEL_TEXT = "vinai/phobert-base"
 BATCH_SIZE = 8
